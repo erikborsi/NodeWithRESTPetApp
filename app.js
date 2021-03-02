91ab8fd4d202ef97ae47e028439330199b0c6ebc
Erik Borsi <erikborsi@users.noreply.github.com>
2021-03-02 15:49:03 +0000

    added cors

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var petsRouter = require('./routes/pets');
+var cors = require('cors')
 
 var app = express();
+
+//enable cross origin requests. 
+app.use(cors());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
