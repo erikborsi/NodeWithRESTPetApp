e4637b899bc6adcadd339b2dc97d5300949e6c81
Erik Borsi <erikborsi@users.noreply.github.com>
2021-03-02 14:50:59 +0000

    first commit

---
+++ routes/pets.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const petData = require('../data/pets.json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.json(petData);
+});
+
+router.put('/:id', function(req, res, next) {
+    res.render('pets', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+    res.render('pets', { title: 'Express' });
+});
+
+router.delete('/:id', function(req, res, next) {
+    res.render('pets', { title: 'Express' });
+});
+
+module.exports = router;
